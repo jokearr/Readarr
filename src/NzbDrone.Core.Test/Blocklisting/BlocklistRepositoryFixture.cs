NzbDrone/NzbDrone

New: Renamed Blacklist to Blocklist

src/NzbDrone.Core.Test/Blocklisting/BlocklistRepositoryFixture.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
-using NzbDrone.Core.Blacklisting;
+using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.Qualities;
 using NzbDrone.Core.Test.Framework;
 
-namespace NzbDrone.Core.Test.Blacklisting
+namespace NzbDrone.Core.Test.Blocklisting
 {
     [TestFixture]
-    public class BlacklistRepositoryFixture : DbTest<BlacklistRepository, Blacklist>
+    public class BlocklistRepositoryFixture : DbTest<BlocklistRepository, Blocklist>
     {
-        private Blacklist _blacklist;
+        private Blocklist _blocklist;
 
         [SetUp]
         public void Setup()
         {
-            _blacklist = new Blacklist
+            _blocklist = new Blocklist
                      {
                          SeriesId = 12345,
                          EpisodeIds = new List<int> { 1 },
@@ -30,24 +30,24 @@ namespace NzbDrone.Core.Test.Blacklisting
         [Test]
         public void should_be_able_to_write_to_database()
         {
-            Subject.Insert(_blacklist);
+            Subject.Insert(_blocklist);
             Subject.All().Should().HaveCount(1);
         }
 
         [Test]
         public void should_should_have_episode_ids()
         {
-            Subject.Insert(_blacklist);
+            Subject.Insert(_blocklist);
 
-            Subject.All().First().EpisodeIds.Should().Contain(_blacklist.EpisodeIds);
+            Subject.All().First().EpisodeIds.Should().Contain(_blocklist.EpisodeIds);
         }
 
         [Test]
-        public void should_check_for_blacklisted_title_case_insensative()
+        public void should_check_for_blocklisted_title_case_insensative()
         {
-            Subject.Insert(_blacklist);
+            Subject.Insert(_blocklist);
 
-            Subject.BlacklistedByTitle(_blacklist.SeriesId, _blacklist.SourceTitle.ToUpperInvariant()).Should().HaveCount(1);
+            Subject.BlocklistedByTitle(_blocklist.SeriesId, _blocklist.SourceTitle.ToUpperInvariant()).Should().HaveCount(1);
         }
     }
 }

src/NzbDrone.Core.Test/Blocklisting/BlocklistServiceFixture.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
-using NzbDrone.Core.Blacklisting;
+using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.Download;
 using NzbDrone.Core.Qualities;
 using NzbDrone.Core.Test.Framework;
 
-namespace NzbDrone.Core.Test.Blacklisting
+namespace NzbDrone.Core.Test.Blocklisting
 {
     [TestFixture]
-    public class BlacklistServiceFixture : CoreTest<BlacklistService>
+    public class BlocklistServiceFixture : CoreTest<BlocklistService>
     {
         private DownloadFailedEvent _event;
 
@@ -39,8 +39,8 @@ namespace NzbDrone.Core.Test.Blacklisting
         {
             Subject.Handle(_event);
 
-            Mocker.GetMock<IBlacklistRepository>()
-                .Verify(v => v.Insert(It.Is<Blacklist>(b => b.EpisodeIds == _event.EpisodeIds)), Times.Once());
+            Mocker.GetMock<IBlocklistRepository>()
+                .Verify(v => v.Insert(It.Is<Blocklist>(b => b.EpisodeIds == _event.EpisodeIds)), Times.Once());
         }
 
         [Test]
@@ -51,8 +51,8 @@ namespace NzbDrone.Core.Test.Blacklisting
             _event.Data.Remove("size");
             _event.Data.Remove("protocol");
 
-            Mocker.GetMock<IBlacklistRepository>()
-                .Verify(v => v.Insert(It.Is<Blacklist>(b => b.EpisodeIds == _event.EpisodeIds)), Times.Once());
+            Mocker.GetMock<IBlocklistRepository>()
+                .Verify(v => v.Insert(It.Is<Blocklist>(b => b.EpisodeIds == _event.EpisodeIds)), Times.Once());
         }
     }
 }

src/NzbDrone.Core/DecisionEngine/Specifications/BlocklistSpecification.cs
@@ -1,18 +1,18 @@
 using NLog;
-using NzbDrone.Core.Blacklisting;
+using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.DecisionEngine.Specifications
 {
-    public class BlacklistSpecification : IDecisionEngineSpecification
+    public class BlocklistSpecification : IDecisionEngineSpecification
     {
-        private readonly IBlacklistService _blacklistService;
+        private readonly IBlocklistService _blocklistService;
         private readonly Logger _logger;
 
-        public BlacklistSpecification(IBlacklistService blacklistService, Logger logger)
+        public BlocklistSpecification(IBlocklistService blocklistService, Logger logger)
         {
-            _blacklistService = blacklistService;
+            _blocklistService = blocklistService;
             _logger = logger;
         }
 
@@ -21,10 +21,10 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
         public Decision IsSatisfiedBy(RemoteEpisode subject, SearchCriteriaBase searchCriteria)
         {
-            if (_blacklistService.Blacklisted(subject.Series.Id, subject.Release))
+            if (_blocklistService.Blocklisted(subject.Series.Id, subject.Release))
             {
-                _logger.Debug("{0} is blacklisted, rejecting.", subject.Release.Title);
-                return Decision.Reject("Release is blacklisted");
+                _logger.Debug("{0} is blocklisted, rejecting.", subject.Release.Title);
+                return Decision.Reject("Release is blocklisted");
             }
 
             return Decision.Accept();

jokearr/Readarr

(cherry picked from commit ead1371846b1f19cd49928052be0128bf7ccd41f)

src/NzbDrone.Core.Test/Blocklisting/BlocklistRepositoryFixture.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
-using NzbDrone.Core.Blacklisting;
+using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.Qualities;
 using NzbDrone.Core.Test.Framework;
 
-namespace NzbDrone.Core.Test.Blacklisting
+namespace NzbDrone.Core.Test.Blocklisting
 {
     [TestFixture]
-    public class BlacklistRepositoryFixture : DbTest<BlacklistRepository, Blacklist>
+    public class BlocklistRepositoryFixture : DbTest<BlocklistRepository, Blocklist>
     {
-        private Blacklist _blacklist;
+        private Blocklist _blocklist;
 
         [SetUp]
         public void Setup()
         {
-            _blacklist = new Blacklist
+            _blocklist = new Blocklist
             {
                 AuthorId = 12345,
                 BookIds = new List<int> { 1 },
@@ -30,24 +30,24 @@ namespace NzbDrone.Core.Test.Blacklisting
         [Test]
         public void should_be_able_to_write_to_database()
         {
-            Subject.Insert(_blacklist);
+            Subject.Insert(_blocklist);
             Subject.All().Should().HaveCount(1);
         }
 
         [Test]
         public void should_should_have_book_ids()
         {
-            Subject.Insert(_blacklist);
+            Subject.Insert(_blocklist);
 
-            Subject.All().First().BookIds.Should().Contain(_blacklist.BookIds);
+            Subject.All().First().BookIds.Should().Contain(_blocklist.BookIds);
         }
 
         [Test]
-        public void should_check_for_blacklisted_title_case_insensative()
+        public void should_check_for_blocklisted_title_case_insensative()
         {
-            Subject.Insert(_blacklist);
+            Subject.Insert(_blocklist);
 
-            Subject.BlacklistedByTitle(_blacklist.AuthorId, _blacklist.SourceTitle.ToUpperInvariant()).Should().HaveCount(1);
+            Subject.BlocklistedByTitle(_blocklist.AuthorId, _blocklist.SourceTitle.ToUpperInvariant()).Should().HaveCount(1);
         }
     }
 }

src/NzbDrone.Core.Test/Blocklisting/BlocklistServiceFixture.cs
@@ -2,15 +2,15 @@ using System;
 using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
-using NzbDrone.Core.Blacklisting;
+using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.Download;
 using NzbDrone.Core.Qualities;
 using NzbDrone.Core.Test.Framework;
 
-namespace NzbDrone.Core.Test.Blacklisting
+namespace NzbDrone.Core.Test.Blocklisting
 {
     [TestFixture]
-    public class BlacklistServiceFixture : CoreTest<BlacklistService>
+    public class BlocklistServiceFixture : CoreTest<BlocklistService>
     {
         private DownloadFailedEvent _event;
 
@@ -39,8 +39,8 @@ namespace NzbDrone.Core.Test.Blacklisting
         {
             Subject.Handle(_event);
 
-            Mocker.GetMock<IBlacklistRepository>()
-                .Verify(v => v.Insert(It.Is<Blacklist>(b => b.BookIds == _event.BookIds)), Times.Once());
+            Mocker.GetMock<IBlocklistRepository>()
+                .Verify(v => v.Insert(It.Is<Blocklist>(b => b.BookIds == _event.BookIds)), Times.Once());
         }
 
         [Test]
@@ -51,8 +51,8 @@ namespace NzbDrone.Core.Test.Blacklisting
             _event.Data.Remove("size");
             _event.Data.Remove("protocol");
 
-            Mocker.GetMock<IBlacklistRepository>()
-                .Verify(v => v.Insert(It.Is<Blacklist>(b => b.BookIds == _event.BookIds)), Times.Once());
+            Mocker.GetMock<IBlocklistRepository>()
+                .Verify(v => v.Insert(It.Is<Blocklist>(b => b.BookIds == _event.BookIds)), Times.Once());
         }
     }
 }

src/NzbDrone.Core/DecisionEngine/Specifications/BlocklistSpecification.cs
@@ -1,18 +1,18 @@
 using NLog;
-using NzbDrone.Core.Blacklisting;
+using NzbDrone.Core.Blocklisting;
 using NzbDrone.Core.IndexerSearch.Definitions;
 using NzbDrone.Core.Parser.Model;
 
 namespace NzbDrone.Core.DecisionEngine.Specifications
 {
-    public class BlacklistSpecification : IDecisionEngineSpecification
+    public class BlocklistSpecification : IDecisionEngineSpecification
     {
-        private readonly IBlacklistService _blacklistService;
+        private readonly IBlocklistService _blocklistService;
         private readonly Logger _logger;
 
-        public BlacklistSpecification(IBlacklistService blacklistService, Logger logger)
+        public BlocklistSpecification(IBlocklistService blocklistService, Logger logger)
         {
-            _blacklistService = blacklistService;
+            _blocklistService = blocklistService;
             _logger = logger;
         }
 
@@ -21,10 +21,10 @@ namespace NzbDrone.Core.DecisionEngine.Specifications
 
         public Decision IsSatisfiedBy(RemoteBook subject, SearchCriteriaBase searchCriteria)
         {
-            if (_blacklistService.Blacklisted(subject.Author.Id, subject.Release))
+            if (_blocklistService.Blocklisted(subject.Author.Id, subject.Release))
             {
-                _logger.Debug("{0} is blacklisted, rejecting.", subject.Release.Title);
-                return Decision.Reject("Release is blacklisted");
+                _logger.Debug("{0} is blocklisted, rejecting.", subject.Release.Title);
+                return Decision.Reject("Release is blocklisted");
             }
 
             return Decision.Accept();